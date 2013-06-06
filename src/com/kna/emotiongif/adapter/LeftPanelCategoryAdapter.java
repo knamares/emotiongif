d97cb48fc10fe89fed324ad2466b319285b19704
Pablo Cañamares Ullan <knamares@gmail.com>
2013-06-06 16:44:03 +0200

    General Skeleton App

---
+++ src/com/kna/emotiongif/adapter/LeftPanelCategoryAdapter.java
@@ -0,0 +1,56 @@
+package com.kna.emotiongif.adapter;
+
+import java.util.List;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.kna.emotiongif.R;
+import com.kna.emotiongif.data.Category;
+
+public class LeftPanelCategoryAdapter extends ArrayAdapter<Category> {
+
+	private final Activity			context;
+	private final List<Category>	categories;
+
+	static class ViewHolder {
+		public TextView		text;
+		public ImageView	image;
+	}
+
+	public LeftPanelCategoryAdapter(Activity context, int textViewResourceId, List<Category> categories) {
+		super(context, textViewResourceId, categories);
+		this.context = context;
+		this.categories = categories;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View rowView = convertView;
+		if (rowView == null) {
+			LayoutInflater inflater = context.getLayoutInflater();
+			rowView = inflater.inflate(R.layout.leftpanelitem, null);
+			ViewHolder viewHolder = new ViewHolder();
+			viewHolder.text = (TextView) rowView.findViewById(R.id.leftpanelitemtext);
+			viewHolder.image = (ImageView) rowView.findViewById(R.id.leftpanelitemimage);
+			rowView.setTag(viewHolder);
+		}
+
+		ViewHolder holder = (ViewHolder) rowView.getTag();
+
+		// Set text
+		String text = categories.get(position).getName();
+		holder.text.setText(text);
+
+		// Set image
+		holder.image.setImageResource(R.drawable.ic_launcher);
+
+		return rowView;
+	}
+
+}
---
+++ src/com/kna/emotiongif/data/DataManager.java
@@ -0,0 +1,55 @@
+package com.kna.emotiongif.data;
+
+import java.util.ArrayList;
+
+public class DataManager {
+
+	@SuppressWarnings("serial")
+	public static ArrayList<Category>	CATEGORIES	= new ArrayList<Category>() {
+														{
+															add(new Category(1, "Affection", "emotions/affection/"));
+															add(new Category(2, "Anger", "emotions/anger/"));
+															add(new Category(3, "Anguish", "emotions/anguish/"));
+															add(new Category(4, "Annoyance", "emotions/annoyance/"));
+															add(new Category(5, "Anxiety", "emotions/anxiety/"));
+															add(new Category(6, "Apathy", "emotions/apathy/"));
+															add(new Category(7, "Boredom", "emotions/boredom/"));
+															add(new Category(8, "Confusion", "emotions/confusion/"));
+															add(new Category(9, "Contempt", "emotions/contempt/"));
+															add(new Category(10, "Contentment", "emotions/contentment/"));
+															add(new Category(11, "Depression", "emotions/depression/"));
+															add(new Category(12, "Despair", "emotions/despair/"));
+															add(new Category(13, "Disgust", "emotions/disgust/"));
+															add(new Category(14, "Distrust", "emotions/distrust/"));
+															add(new Category(15, "Ecstasy", "emotions/ecstasy/"));
+															add(new Category(16, "Embarrassment", "emotions/embarrassment/"));
+															add(new Category(17, "Euphoria", "emotions/euphoria/"));
+															add(new Category(18, "Excitement", "emotions/excitement/"));
+															add(new Category(19, "Fear", "emotions/fear/"));
+															add(new Category(20, "Frustration", "emotions/frustration/"));
+															add(new Category(21, "Grief", "emotions/grief/"));
+															add(new Category(22, "Happiness", "emotions/happiness/"));
+															add(new Category(23, "Hatred", "emotions/hatred/"));
+															add(new Category(24, "Horror", "emotions/horror/"));
+															add(new Category(25, "Hostility", "emotions/hostility/"));
+															add(new Category(26, "Indifference", "emotions/indifference/"));
+															add(new Category(27, "Jealousy", "emotions/jealousy/"));
+															add(new Category(28, "Joy", "emotions/joy/"));
+															add(new Category(29, "Loathing", "emotions/loathing/"));
+															add(new Category(30, "Loneliness", "emotions/loneliness/"));
+															add(new Category(31, "Love", "emotions/love/"));
+															add(new Category(32, "Panic", "emotions/panic/"));
+															add(new Category(33, "Pride", "emotions/pride/"));
+															add(new Category(34, "Rage", "emotions/rage/"));
+															add(new Category(35, "Sadness", "emotions/sadness/"));
+															add(new Category(36, "Satisfaction", "emotions/satisfaction/"));
+															add(new Category(37, "Shock", "emotions/shock/"));
+															add(new Category(38, "Suffering", "emotions/suffering/"));
+															add(new Category(39, "Surprise", "emotions/surprise/"));
+															add(new Category(40, "Terror", "emotions/terror/"));
+															add(new Category(41, "Trust", "emotions/trust/"));
+															add(new Category(42, "Worry", "emotions/worry/"));
+														}
+													};
+
+}
